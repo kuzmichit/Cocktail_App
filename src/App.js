dced3da9565dac2c21d47f428de74a38f54091f1
kuzia <kuzmichit@gmail.com>
2024-06-06 20:34:06 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 
 import React from "react";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import {bootstrap} from 'bootstrap';
+import {
+BrowserRouter as Router,
+Routes,
+Route,
+Link,
+} from "react-router-dom";
+
 
 class App extends React.Component {
   
   render() {
+  
     const { name } = this.props;
-    console.log('test console')
+    console.log(document.getElementById('go'))
+    
     return (
-      <div style={{'marginTop':'300px'}}>
-        <h1>
-          Hello finally culo non da
-        </h1>
-      </div>
-    );
+      <Router>
+        <div className='text-primary' id='go'>
+        ggggggggg
+          <Row>
+          <Col>
+            Tab to edit
+          </Col>
+          
+          <Col>
+            Tab to edit
+          </Col>
+          </Row>
+        </div>
+      </Router>
+  )
   }
 }
 
--- src/CocktailApp/Navbar.jsx
+++ src/CocktailApp/Navbar.jsx
@@ -1,19 +1,29 @@
 
-import {
-BrowserRouter,
-Routes,
-Route,
-Link,
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
 return (
-<BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="users/*" element={<Users />} />
-  </Routes>
-</BrowserRouter>
+  <nav>
+  <ul className={navbar}>
+    <li className={'logo'}>Logo</li>
+      <ul className={'linguages'}>
+        <li>EN</li>
+        <li>IT</li>
+      </ul>
+  </ul>
+  </nav>
 );
 }
+
+export default Navbar;
+
+ <Container style={{'marginTop':'300px'}}>
+        <Row>
+          <Col sm='2'>Logo</Col>
+          <Col sm='6'> main navi</Col>
+        </Row>
+          <h1>
+          Hello finally culo non da
+          </h1>
+        </Container>
